52defa9ec53d38576c32b8040d7cbe31ff664076
Т̷е̷н̷ьSAQM <kurichich5@gmail.com>
2025-06-08 20:42:06 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 PORT=3429 # Port to run the server on
 echo "EXTERNAL_PORT=$PORT" >.env
 
-eval "git clone https://github.com/coddrago/Hikka"
+eval "git clone https://github.com/Viciu3/Shadow"
 cd Hikka
 
 touch hikka-install.log
