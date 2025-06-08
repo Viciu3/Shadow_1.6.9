1951f12ba25b1dae8becaec24985b6fc180d6eb0
Т̷е̷н̷ьSAQM <kurichich5@gmail.com>
2025-06-08 20:24:11 +0200

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -2,8 +2,8 @@
 
 echo -e "\033[2J\033[3;1f"
 
-eval "cat ~/Heroku/assets/download.txt"
-printf "\n\n\033[1;35mHeroku is being installed... ✨\033[0m"
+eval "cat ~/Shadow/assets/download.txt"
+printf "\n\n\033[1;35mShadow is being installed... ✨\033[0m"
 
 echo -e "\n\n\033[0;96mInstalling base packages...\033[0m"
 
@@ -24,7 +24,7 @@ printf "\r\033[K\033[0;32mPillow installed!\e[0m\n"
 echo -e "\033[0;96mDownloading source code...\033[0m"
 
 eval "rm -rf ~/Hikka 2>/dev/null"
-eval "cd && git clone https://github.com/coddrago/Heroku && cd Heroku"
+eval "cd && git clone https://github.com/Viciu3/Shadow && cd Shadow"
 
 echo -e "\033[0;96mSource code downloaded!...\033[0m\n"
 printf "\r\033[0;34mInstalling requirements...\e[0m"
@@ -37,7 +37,7 @@ if [[ -z "${NO_AUTOSTART}" ]]; then
     printf "\n\r\033[0;34mConfiguring autostart...\e[0m"
 
     eval "echo '' > ~/../usr/etc/motd &&
-    echo 'clear && cd ~/Heroku && python3 -m hikka' > ~/.bash_profile"
+    echo 'clear && cd ~/Shadow && python3 -m hikka' > ~/.bash_profile"
 
     printf "\r\033[K\033[0;32mAutostart enabled!\e[0m\n"
 fi
